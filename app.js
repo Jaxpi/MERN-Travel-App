7edcdd2adff637c36f8f994adac6cb595d80f9ac
Jax I <jaxpi85@gmail.com>
2023-07-19 17:14:06 -0400

    trying to debug blank webpage

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+const cors = require('cors');
+
 const app = express();
+
+const books = require('./routes/api/books');
 
 connectDB();
 
+app.use(cors({ origin: true, credentials: true }));
+
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('Travel App Coming Soon'));
+
+app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
